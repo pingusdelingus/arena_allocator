9ba1732549d76030da6db5720c3fd3009100ded5
pingusdelingus <alphaspammail2016@gmail.com>
2026-01-23 16:59:00 -0500

    made arena(handler) and arenablocks for doubly linked lists of memory

--- Arena.c
+++ Arena.c
@@ -7,7 +7,182 @@ static const i64 INITIAL_SIZE = 300; // 300 bytes
 
 static const i8 ALIGNMENT_BYTES = 16;
 
-void ArenaDebugPrint(Arena* arena) {
+
+ArenaBlock* AllocArenaBlock(void)
+{
+  ArenaBlock* arena = (ArenaBlock*) malloc(sizeof(ArenaBlock));  
+  if (arena == NULL){
+    return NULL;
+  }
+  
+  arena->buffer = (char*) malloc(INITIAL_SIZE);
+  if (arena->buffer == NULL){
+    return NULL;
+  }
+  arena->isFull = 0;
+  arena->next = NULL;
+  arena->max_capacity = INITIAL_SIZE; 
+  arena->used_capacity = 0;
+  arena->ptr = 0; // point at first index !
+  memset(arena->buffer, 0, arena->max_capacity); // set zeroes 
+  return arena; // will not get deleted after scope ends
+}// end of AllocArenaBlock (constructor)
+
+
+
+void ReleaseArenaBlocks(ArenaBlock * a)
+{
+  struct ArenaBlock* curr  = a;
+
+  while (curr != NULL){
+      struct ArenaBlock* next = curr->next;
+      
+        // free char* buff
+      free(curr->buffer);
+
+      // free arena itself
+      free(a);
+
+      // iterate   
+     curr = next;
+  }// end of while
+
+}// end of ReleaseArenaBlock (~destructor)
+
+
+void* ArenaBlockPush(ArenaBlock* arena, i64 s, b8 doSizeCheck)
+{
+  if (doSizeCheck) {
+
+        if (arena->isFull){
+          if (arena->next != NULL){
+          return ArenaBlockPush(arena->next, s, doSizeCheck);
+        } 
+      }
+
+      assert ((arena->ptr + 1 ) % ALIGNMENT_BYTES == 0 );
+      i64 size = s;
+
+      if (size % ALIGNMENT_BYTES != 0){
+        size = (size) + (ALIGNMENT_BYTES - (size % ALIGNMENT_BYTES) );
+      }
+
+
+      i64 req_plus_used = size + arena->used_capacity; 
+      if (req_plus_used > arena->max_capacity){
+          // make new arena for next ptr
+          ArenaBlock* nextArenaBlock = AllocArenaBlock();
+          assert(nextArenaBlock != NULL);
+
+          arena->isFull = 1;
+          arena->next = nextArenaBlock;
+          return ArenaBlockPush(arena->next, size, doSizeCheck);
+      }
+
+  }// end of if do sizecheck
+ 
+  i64 size = s; 
+  char* current_ptr = &(arena->buffer[arena->ptr]); // return this in a sec
+  arena->ptr += size;
+  arena->used_capacity += size;
+  return current_ptr; 
+}// end of ArenaBlockPush
+
+
+void* ArenaBlockPushZeroes(ArenaBlock* arena, i64 s, b8 doSizeCheck)
+{
+  if (doSizeCheck){
+      i64 size = s;
+      if (size % ALIGNMENT_BYTES != 0){
+        size = (size) + (ALIGNMENT_BYTES - (size % ALIGNMENT_BYTES) );
+      }
+      b8 dontSizeCheck = 0;
+
+      if (arena->isFull){
+        if (arena->next != NULL){
+          ArenaBlockPushZeroes(arena->next, size, dontSizeCheck);
+        }
+      }
+
+      i64 req_plus_used = size + arena->used_capacity; 
+      if (req_plus_used > arena->max_capacity){
+
+        ArenaBlock* nextArenaBlock = AllocArenaBlock();
+        assert(nextArenaBlock != NULL);
+
+        arena->max_capacity = arena->used_capacity;
+        arena->isFull = 1;
+        arena ->next = nextArenaBlock; 
+
+        return ArenaBlockPushZeroes(arena->next, size , dontSizeCheck);
+      }
+  } // end of do size check
+
+  i64 size = s;
+  i64 start_num = arena->ptr;
+
+  char* current_ptr = &(arena->buffer[arena->ptr]);
+
+  for (i64 i = start_num; i  < arena->ptr + size; i++){
+    arena->buffer[i] = '\0';
+  }// end of for
+
+  arena->ptr += size;
+  arena->used_capacity += size;
+
+  return current_ptr;
+}// end of ArenaBlockPushZeroes
+
+
+void ArenaBlockPop(ArenaBlock* arena, i64 size)
+{
+  i64 used_minus_size = arena->used_capacity - size;
+  
+  i64 delete_size = size;
+  b8 remove_too_much = 0;
+  i64 stop_index = 0;  
+
+  if (used_minus_size < 0){
+      remove_too_much = 1;  
+      delete_size = arena->used_capacity;
+      stop_index = 0;
+        
+      if (arena->prev != NULL){
+          ArenaBlockPop(arena->prev, used_minus_size);
+    }
+
+  }else{
+      stop_index = arena->ptr - delete_size; // should be non-negative
+      assert(stop_index >= 0);
+  }
+
+  //zeroing out memory
+  for (i64 i = arena->ptr - 1; i >= stop_index; i--){ arena->buffer[i] = '\0'; }  
+
+  if (remove_too_much){
+      arena->used_capacity = 0;
+      arena->ptr = 0;
+    }
+
+  else {
+      arena->used_capacity -= size;  
+      arena->ptr -= size;  
+  }
+}// end of ArenaBlockPop
+
+
+i64 GetArenaBlockSize(ArenaBlock* arena)
+{
+  return arena->used_capacity;
+}// end of GetArenaBlockSize
+
+
+typedef struct{
+  i32 x;
+  i32 y;
+} Vec2;
+
+void ArenaBlockDebugPrint(ArenaBlock* arena) {
     printf("-------------------\n\n");
     printf("\nARENA DEBUG \n");
     printf("Pointer: %lld | Used: %lld | Max: %lld\n", 
@@ -28,165 +203,118 @@ void ArenaDebugPrint(Arena* arena) {
     }
     printf("]\n");
     printf("-------------------\n\n");
-}// end of ArenaDebugPrint
+}// end of ArenaBlockDebugPrint
 
 
-Arena* ArenaAlloc(void)
+
+
+Arena* ArenaConstruct(void)
 {
-  Arena* arena = (Arena*) malloc(sizeof(Arena));  
-  if (arena == NULL){
-    return NULL;
-  }
+  Arena * a = (Arena*) malloc(sizeof(Arena));
+  a->genesis = AllocArenaBlock();
+  a->current = a->genesis;
   
-  arena->buffer = (char*) malloc(INITIAL_SIZE);
-  if (arena->buffer == NULL){
-    return NULL;
-  }
-
-  arena->max_capacity = INITIAL_SIZE; // i32 to i64 conversion ? 
-  arena->used_capacity = 0;
-  arena->ptr = 0; // point at first index !
-  memset(arena->buffer, 0, arena->max_capacity); // set zeroes 
-  return arena; // will not get deleted after scope ends
-}// end of ArenaAlloc (constructor)
-
-Arena*  ArenaRealloc(Arena* a)
-{
-  i64 size = a->max_capacity;
-  
-  char* new_buff = (char* )realloc (a->buffer, 2 * size);
-  if (new_buff == NULL){
-    return NULL;
-  }
-  a-> max_capacity *= 2;
-  
-  a->buffer = new_buff;
-
   return a;
+}
 
-}// end of ArenaRealloc
 
-
-void ReleaseArena(Arena * a)
+void ArenaDestruct(Arena* a)
 {
-  // free char* buff
-  free(a->buffer);
+  ReleaseArenaBlocks(a->genesis);
+}
 
-  // free arena itself
-  free(a);
-}// end of ReleaseArena (~destructor)
-
-
-void* ArenaPush(Arena* arena, i64 s)
+void* ArenaPush(Arena* a, i64 s)
 {
   i64 size = s;
   if (size % ALIGNMENT_BYTES != 0){
     size = (size) + (ALIGNMENT_BYTES - (size % ALIGNMENT_BYTES) );
   }
-
-  i64 req_plus_used = size + arena->used_capacity; 
-  if (req_plus_used > arena->max_capacity){
-    a = ArenaRealloc(arena);
-  }
-
-  char* current_ptr = &(arena->buffer[arena->ptr]); // return this in a bit
-  arena->ptr += size;
-  arena->used_capacity += size;
-  return current_ptr; 
-}// end of ArenaPush
-
-
-void* ArenaPushZeroes(Arena* arena, i64 s)
-{
-  i64 size = s;
-  if (size % ALIGNMENT_BYTES != 0){
-    size = (size) + (ALIGNMENT_BYTES - (size % ALIGNMENT_BYTES) );
-  }
-
-  i64 req_plus_used = size + arena->used_capacity; 
-  if (req_plus_used > arena->max_capacity){
-    perror("trying to request more bytes than available");
-    return NULL;
-  }
-
-  i64 start_num = arena->ptr;
-
-  char* current_ptr = &(arena->buffer[arena->ptr]);
-
-  for (i64 i = start_num; i  < arena->ptr + size; i++){
-    arena->buffer[i] = '\0';
-  }// end of for
-
-  arena->ptr += size;
-  arena->used_capacity += size;
-
-  return current_ptr;
-}// end of ArenaPushZeroes
-
-
-void ArenaPop(Arena* arena, i64 size)
-{
-  i64 used_minus_size = arena->used_capacity - size;
   
-  i64 delete_size = size;
-  b8 remove_too_much = 0;
-  i64 stop_index = 0;  
-  if (used_minus_size < 0){
-    remove_too_much = 1;  
-    delete_size = arena->used_capacity;
-    stop_index = 0;
-  }else{
-    stop_index = arena->ptr - delete_size; // should be non-zero 
-    assert(stop_index != 0);
+  b8 dontsizecheck = 0;
+
+  ArenaBlock* curr = a->current;
+  i64 req_plus_used = size + curr->used_capacity; 
+
+  if (!curr->isFull && req_plus_used < curr->max_capacity){
+    dontsizecheck = 0;
+   return ArenaBlockPush(curr, s, dontsizecheck);
   }
 
-  for (i64 i = arena->ptr - 1; i >= stop_index; i--){
-      arena->buffer[i] = '\0';
-  } 
+  // full or requesting too much memory 
+  // make new arena for next ptr
+  ArenaBlock* nextArenaBlock = AllocArenaBlock();
+  assert(nextArenaBlock != NULL);
 
-  if (remove_too_much){
-  arena->used_capacity = 0;
-  arena->ptr = 0;
-  }
-  else {
-  arena->used_capacity -= size;  
-  arena->ptr -= size;  
-  }
-}// end of ArenaPop
+  curr->isFull = 1;
+  curr ->max_capacity = curr->used_capacity;
+
+  curr->next = nextArenaBlock;
+  nextArenaBlock->prev = curr;
+
+  a->current = nextArenaBlock;
+  assert(nextArenaBlock->next == NULL);
+  return ArenaBlockPush(a->current, size, dontsizecheck);
+}
 
 
-i64 GetArenaSize(Arena* arena)
+void* ArenaPushZeroes(Arena* a, i64 s)
 {
-  return arena->used_capacity;
-}// end of GetArenaSize
+  i64 size = s;
+  if (size % ALIGNMENT_BYTES != 0){
+    size = (size) + (ALIGNMENT_BYTES - (size % ALIGNMENT_BYTES) );
+  }
+  
+  b8 dontsizecheck = 0;
+
+  ArenaBlock* curr = a->current;
+  i64 req_plus_used = size + curr->used_capacity; 
+
+  if (!curr->isFull && req_plus_used < curr->max_capacity){
+    dontsizecheck = 0;
+   return ArenaBlockPushZeroes(curr, s, dontsizecheck);
+  }
+
+  // full or requesting too much memory 
+  // make new arena for next ptr
+  ArenaBlock* nextArenaBlock = AllocArenaBlock();
+  assert(nextArenaBlock != NULL);
+
+  curr->isFull = 1;
+  curr ->max_capacity = curr->used_capacity;
+
+  curr->next = nextArenaBlock;
+  nextArenaBlock->prev = curr;
+
+  a->current = nextArenaBlock;
+  assert(nextArenaBlock->next == NULL);
+  return ArenaBlockPushZeroes(a->current, size, dontsizecheck);
+//  return ArenaBlockPushZeroes(a->current);
+}
 
 
-typedef struct{
-  i32 x;
-  i32 y;
-} Vec2;
+void ArenaPop(Arena* a, i64 size)
+{
+ ArenaBlock* current = a->current;
+  ArenaBlockPop(current, size);
+}
+
+i64 GetArenaSize(Arena* a)
+{
+  i64 size = 0;
+
+ ArenaBlock* current = a->genesis;
+
+  while(current != NULL){
+    size += current->used_capacity;
+      current = current->next; 
+  }
+  return size;
+}// GetArenaSize
+
 
 
 int main(void)
 {
-  Arena* a = ArenaAlloc();
-  printf("pusing\n");
-  char* e = ArenaPush(a, 60);
-
-  memset(e, 'e', 60);
-  printf("popping\n");
-  ArenaPop(a, 60);
- 
-  printf("popping everything \n");
-  ArenaPop(a, 500);
-
-  char* h = ArenaPush(a, 5);
-  strcpy(h, "help!");
-  ArenaDebugPrint(a);
-  ArenaPop(a, 5);
-  ArenaDebugPrint(a);
-  ArenaPop(a, 500);
-  ArenaDebugPrint(a);
-  ReleaseArena(a);
+Arena * a = ArenaConstruct();
   
-}
+}// end of main
--- arena.h
+++ arena.h
@@ -1,33 +1,50 @@
 // @author: esteban morales
 
 
-typedef struct {
+typedef struct ArenaBlock {
+  struct ArenaBlock* next;
+  struct ArenaBlock* prev;
   char* buffer;
   i64 max_capacity;
   i64 used_capacity;
   i64 ptr;
+  b8 isFull;
+} ArenaBlock;
+
+typedef struct{
+  ArenaBlock* current;
+  ArenaBlock* genesis;
 } Arena;
 
 
 // constructor 
-Arena* ArenaAlloc(void );
+ArenaBlock* AllocArenaBlock(void );
 
 // ~destructor
-void ReleaseArena(Arena * a); 
+void ReleaseArenaBlocks(ArenaBlock * a); 
 
-// realloc (double size if full)
-void ArenaRellaoc(Arena * a);
 //push bytes
-void* ArenaPush(Arena* arena, i64 size);
+void* ArenaBlockPush(ArenaBlock* arena, i64 size, b8 doSizeCheck);
 
 // push zeroe'd bytes
-void* ArenaPushZeroes(Arena* arena, i64 size);
+void* ArenaBlockPushZeroes(ArenaBlock* arena, i64 size, b8 doSizeCheck);
 
 //pop bytes from arena
-void ArenaPop(Arena* arena, i64 size);
+void ArenaBlockPop(ArenaBlock* arena, i64 size);
 
 //get num bytes used currentlty
-i64 GetArenaSize(Arena* arena);
+i64 GetArenaBlockSize(ArenaBlock* arena);
 
 
  
+
+
+Arena* ArenaConstruct(void);
+void ArenaDestruct(Arena* a);
+
+void* ArenaPush(Arena* a, i64 size);
+void* ArenaPushZeroes(Arena* a, i64 size);
+
+void ArenaPop(Arena* a, i64 size);
+i64 GetArenaSize(Arena* a);
+
